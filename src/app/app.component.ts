96d981716e29679b57b12a971e5a5ce2d6e99c75
Felipe Barcon <felipe.barcon@gmail.com>
2022-10-31 19:03:35 +0100

    creating first app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { POKEMONS } from "./mock-pokemons";
+import { Pokemon } from "./pokemon";
 
 @Component({
   selector: "app-root",
-  template: `<h1>Hello, {{ name }} !</h1>`,
+  template: `<h1>Pokémons</h1>`,
 })
-export class AppComponent {
-  name = "Angular";
+export class AppComponent implements OnInit {
+  pokemons: Pokemon[];
+
+  ngOnInit() {
+    this.pokemons = POKEMONS;
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log("Vous avez cliqué sur " + pokemon.name);
+  }
 }
