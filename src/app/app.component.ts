7d23ad17f2cc42100d03f05a43cc8223a43b46e7
Felipe Barcon <felipe.barcon@gmail.com>
2022-12-14 16:07:41 +0100

    jouer avec les varaiables référencées

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,23 @@ import { Pokemon } from "./pokemon";
 
 @Component({
   selector: "app-root",
-  template: `<h1>Pokémons</h1>`,
+  template: `<h1>Pokémons</h1>
+    <input #box (keyup)="onKey(box.value)" />
+    <p>{{ values }}</p> `,
 })
 export class AppComponent implements OnInit {
   pokemons: Pokemon[];
+  values: string;
 
   ngOnInit() {
     this.pokemons = POKEMONS;
   }
 
+  onKey(value: string) {
+    this.values = value;
+  }
+
   selectPokemon(pokemon: Pokemon) {
-    console.log("Vous avez cliqué sur " + pokemon.name);
+    console.log(`Vous avez cliqué sur ${pokemon.name}`);
   }
 }
